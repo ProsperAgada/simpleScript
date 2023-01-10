08530bbc93a18a5d4cc77927ca701d69f5c986bd
Prosper Agada <prosperagada@nvit.tech>
2023-01-10 13:03:48 +0100

    new update to automate.sh

--- automate.sh
+++ automate.sh
@@ -1,2 +1,153 @@
-#!/bin/bash
-echo sudo apt update; 'date'
+    1  clear
+    2  docker ps
+    3  sudo apt install docker.io
+    4  sudo apt install docker
+    5  clear
+    6  docker ps
+    7  clear
+    8  mkdir nvit
+    9  cd nvit
+   10  git intit
+   11  git init
+   12  ssh-keygen
+   13  cd ¬
+   14  cd ..
+   15  cd ssh
+   16  ls -al
+   17  cd .ssh
+   18  ls
+   19  cat id_rsa.pub 
+   20  clear
+   21  cd ..
+   22  cd nvit
+   23  mkdir git_class
+   24  cd git_class
+   25  git --version
+   26  git config --global user.name "Prosper Agada"
+   27  git config --global user.email "prosperagada@nvit.tech"
+   28  mkdir Bash_class ; cd Bash_class
+   29  git status
+   30  git init
+   31  ll
+   32  git status
+   33  touch automate.py namespace.yaml
+   34  ll
+   35  git status
+   36  git add namespace.yaml
+   37  git status
+   38  git commit -m "My first namespace file"
+   39  git log
+   40  clear
+   41  git status
+   42  cat > namespace.yaml
+   43  vi# namespace.yaml
+   44  vi namespace.yaml
+   45  git status
+   46  git add .
+   47  git commit -m "new update to namespaces and added automate script"
+   48  git log
+   49  git branch -b devteam
+   50  git branch devteam
+   51  git cheackout devteam
+   52  git checkout devteam
+   53  git log
+   54  vi automate.sh
+   55  git add.
+   56  git add .
+   57  git status
+   58  git log
+   59  git commit -m "added bash automation script"
+   60  git log
+   61  git checkout -b backend
+   62  ll
+   63  ls
+   64  clear
+   65  ll
+   66  cd nvit
+   67  ll
+   68  cd git_class
+   69  ll
+   70  ls
+   71  cd bash_class
+   72  cd Bash_class
+   73  ll
+   74  git checkout dev
+   75  git status
+   76  git log
+   77  git checkout devteam
+   78  git status
+   79  git log
+   80  git branch
+   81  git remote -v
+   82  git pull https://github.com/ProsperAgada/simpleScript.git
+   83  git status
+   84  git log
+   85  ll
+   86  git push -u origin main
+   87  git log
+   88  git branch
+   89  git status
+   90  git merge backend
+   91  git checkout master
+   92  git merge devteam
+   93  git log --graph --decourate
+   94  git log --graph --decorate
+   95  git branch
+   96  git branch --delete devteam
+   97  git branch --delete backend
+   98  git branch
+   99  git push origin main
+  100  git push -u origin main
+  101  git push 
+  102  git push main
+  103  git remote add https://github.com/ProsperAgada/simpleScript.git
+  104  git push -u master main
+  105  ll
+  106  git status
+  107  git push main
+  108  git push --set-upstream master master
+  109  git push -u origin https://github.com/ProsperAgada/simpleScript.git
+  110  git push -u origin main
+  111  git push -u origin master
+  112  git add .
+  113  git status
+  114  git push -u origin master
+  115  git remote -v
+  116  git push -u original master
+  117  git push -u origin master
+  118* 
+  119  ssh-keygen
+  120* git 
+  121  ssh-keygen -t ed25519 -C "prosperagada@nvit.tech"
+  122  cat /home/agasprosper/.ssh/id_ed25519
+  123  ssh-add ~/.ssh/id_ed25519
+  124  eval "$(ssh-agent -s)"
+  125  ssh-add ~/.ssh/id_ed25519
+  126  cat /home/agasprosper/.ssh/id_ed25519.pub
+  127  git push -u original master
+  128  git push -u origin master
+  129  git push
+  130  git push main
+  131  git push -u origin --d main
+  132  git push origin main
+  133  git push -u origin master
+  134  git push -u original master
+  135  git --set-upstream origin main
+  136  git push -u original master
+  137  cd ../..
+  138  ll
+  139  cd git_class
+  140  ll
+  141  cd Bash_class
+  142  ll
+  143  vi .git/config
+  144  git remote -v
+  145  git add .
+  146  git commit -m "new commit"
+  147  git status
+  148  git push -u origin main
+  149  git remote -v
+  150  git push -u original main
+  151  git push -u original master
+  152  history
+  153  history > automate.sh
